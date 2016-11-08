82d39315a76d6d3172105fb1630435c488ff66e5
petrero <petrero@gmail.com>
2016-11-08 21:02:09 +0200

    Making database query

--- public_html/Ajax/search.php
+++ public_html/Ajax/search.php
@@ -4,5 +4,12 @@ $connection = mysqli_connect('localhost', 'root', 'mysql', 'AjaxPHP');
 
 $search = $_POST['search'];
 
-echo $search;
+if(!empty($search)){
+  $query = "SELECT * FROM cars WHERE cars LIKE '$search%'";
+  $search_query = mysqli_query($connection, $query);
+  
+  if(!$search_query){
+    die('QUERY FAILED ' . mysqli_error($connection));
+  }
+}
 
