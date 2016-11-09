79edb4727fed465d640f26ad107c97a217adfc09
petrero <petrero@gmail.com>
2016-11-09 21:13:47 +0200

    Reset the form after inserting a car in the database. If search for a car not introduced in database display a message instead the list of available cars.

--- public_html/Ajax/search.php
+++ public_html/Ajax/search.php
@@ -7,6 +7,7 @@ $search = $_POST['search'];
 if(!empty($search)){
   $query = "SELECT * FROM cars WHERE cars LIKE '$search%'";
   $search_query = mysqli_query($connection, $query);
+  $count = mysqli_num_rows($search_query);
   
   if(!$search_query){
     die('QUERY FAILED ' . mysqli_error($connection));
@@ -14,13 +15,17 @@ if(!empty($search)){
   ?>
   <ul class="list-unstyled">
   <?php
-  while($row = mysqli_fetch_array($search_query)){
-    $brand = $row['cars'];
-    echo "<li>{$brand} in stock</li>";
+  if($count <= 0){
+    echo "Sorry we don't have that car available";
+  } else {
+    while($row = mysqli_fetch_array($search_query)){
+      $brand = $row['cars'];
+      echo "<li>{$brand} in stock</li>";
+    }
+    ?>
+    </ul>
+    <?php
   }
-  ?>
-  </ul>
-  <?php
 }
 ?>
 
