23b50b7c2ac0f063ca228579c70c997115b34b46
petrero <petrero@gmail.com>
2016-11-09 20:08:41 +0200

    Inserting data

--- public_html/Ajax/add_cars.php
+++ public_html/Ajax/add_cars.php
@@ -2,6 +2,12 @@
 
 include 'db.php';
 
-if(isset($_POST['car_name'])){
-  echo "DATA RECEIVED";
+if(isset($_POST['car_name']) && !empty(trim($_POST['car_name']))){
+  $car_name = $_POST['car_name'];
+  $query = "INSERT INTO cars(cars) VALUES('$car_name')";
+  $query_car_name = mysqli_query($connection, $query);
+  if(!$query_car_name){
+    die("QUERY FAILED");
+  }
+  header("Location: index.html");
 }
--- public_html/Ajax/db.php
+++ public_html/Ajax/db.php
@@ -1,3 +1,4 @@
 <?php
+ob_start();
 
 $connection = mysqli_connect('localhost', 'root', 'mysql', 'AjaxPHP');
